b772da56e8a41e3c5259fb34bd6816648077fc49
Amaryann <121064694+Amaryann@users.noreply.github.com>
2023-07-18 14:50:54 +0300

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { ContactForm } from "./ContactForm";
+// import { ContactList } from "./ContactList";
 export const App = () => {
   return (
     <div
@@ -10,7 +12,19 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <div>
+  <h1>Phonebook</h1>
+  <ContactForm />
+
+  <h2>Contacts</h2>
+  {/* <Filter /> */}
+  {/* <ContactList /> */}
+</div>
     </div>
   );
 };
+export let state = {
+  contacts: [],
+  name: "",
+  number: "",
+}
---
+++ src/components/ContactList.jsx
@@ -0,0 +1,5 @@
+export const ContactList = () => {
+    return (
+        <ul className="contactlist" id="contactlist"></ul>
+    )
+}
